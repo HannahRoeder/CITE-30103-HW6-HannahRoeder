991a190af4757f842f888481dbb0800df8638b3a
Hannah Roeder <hannah.roeder@tcu.edu>
2025-11-05 11:14:20 -0600

    Added documentation

--- question1.c
+++ question1.c
@@ -1,11 +1,13 @@
+//documentation
 #include <stdio.h>
 
+//main section
 int main() {
 	int length, width;
 	printf("Enter the length: ");
 	scanf("%d", &length);
 	printf("Enter the width: ");
 	scanf("%d", &width);
-
+	//add each of the 2 sides together
 	printf("Perimeter: %d\n", ((2 * width) + (2 * length)));
 }
