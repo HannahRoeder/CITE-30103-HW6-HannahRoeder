cd2a2ca5cc00f049a296ce84968aa79a073269fd
Hannah Roeder <hannah.roeder@tcu.edu>
2025-11-14 14:23:14 -0600

    created a program that prints multiplication tables for a user specified range.

---
+++ question25.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int L, R;
+
+	printf("Enter L: ");
+	scanf("%d", &L);
+
+	printf("Enter R: ");
+	scanf("%d", &R);
+	
+	for (int i = L; i <= R; i++) {
+		printf("Table of %d\n", i);
+
+		for (int j = 1; j <= 10; j++) {
+			printf("%d * %d = %d\n", i, j, i * j);
+		}
+
+		printf("\n");
+	}
+	return 0;
+
+}
